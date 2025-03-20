5a7a4a805cb6ed81bc1e7061028cc74c6ee30147
shubham singhk <shubhamsinghia160@gmail.com>
2025-03-21 01:00:58 +0530

    Db connected

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const ConnectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`);
+
+        console.log(`Connected to MongoDB at ${connectionInstance.connection.host}`);
+
+    } catch (error) {
+
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+
+export default ConnectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import dotenv from "dotenv";
+import ConnectDB from "./db/index.js";
+import express from "express";
 
+dotenv.config();
 
-console.log("hello from  nodemon and shubham");
+const app = express();
 
+ConnectDB();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
 
